0f286a84925f372ca39301c6904f77195be1290f
ryanbmarx <ryanbmarx@gmail.com>
2018-01-27 18:24:44 -0600

    Wire up autocomplete, and style it, too.

--- js/src/app.js
+++ js/src/app.js
@@ -1,18 +1,50 @@
 const inView = require('in-view');
-
+import 'awesomplete';
+import clickTrack from "./click-track.js";
 
 // Let's set our offset to 33% into the viewport. 
 inView.offset(window.innerWidth / 3);
 
 window.addEventListener('DOMContentLoaded', function(e){
-
+	// These waypoints power the navbar buttons
 	inView('.category')
 	    .on('enter', el => {
-
 	        let raceID =  el.getAttribute('id');
-			
 	        document.querySelector('.nav-button--active').classList.remove('nav-button--active');
 	        document.querySelector(`.nav-button[href="#${raceID}"]`).classList.add('nav-button--active');
 	    });
 
+    // ----------------------------
+    // AUTOCOMPLETE ---------------
+    // ----------------------------
+
+	const 	form = document.querySelector('#candidate-lookup'),
+			searchBar = form.querySelector('input'),
+			submitButton = form.querySelector('button');
+
+	// This is the autocomplete tool. It needs the `list` attribute
+	// to be filled with autocomplete options. We'll do that with a 
+	// window variable templated with Jinja on the index page.
+	
+	const auto = new Awesomplete(searchBar, {
+		minChars: 2,
+		maxItems: 20,
+		autoFirst: true,
+		list: window.candidates,
+		replace: function(text){
+			// This puts the readable name into the search bar
+			// this means the awesomplete object. 
+			this.input.value = text.label;
+		}
+	});
+
+	window.addEventListener('awesomplete-selectcomplete', function(e) {
+		// When the user makes an autocompleted selection, take the "value"
+		// and go to that candidate page
+		window.location = `${window.ROOT_URL}/candidates/${e.text.value}.html`;
+
+		// Trigger an omniture link track
+		clickTrack('Elex 2018 - surveys - candidate search');
+	})
 });
+
---
+++ js/src/click-track.js
@@ -0,0 +1,20 @@
+module.exports = function clickTrack(uniquePhrase){
+	try{
+		s.linkTrackVars = "server,prop3,prop20,prop28,prop33,prop34,prop57,eVar3,eVar20,eVar21,eVar34,eVar35,eVar36,eVar37,eVar38,eVar39,eVar51";
+		s.linkTrackEvents = "";
+		s.prop57 = uniquePhrase;
+		s.tl(
+		   // Since we're not actually tracking a link click, use true instead of `this`.  This also supresses a delay
+		   true,
+		   // linkType, 'o' for custom link
+		   'o',
+		   // linkName
+			uniquePhrase,
+		   // variableOverrides
+		   null
+		);
+	}
+	catch (ReferenceError){
+		console.warn('You must be running this locally. *OR* Omniture is not loaded. Skipping analytics.');
+	}
+}
